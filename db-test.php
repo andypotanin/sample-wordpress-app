81277d330fd94283abd6e14fabda76c411307073
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-07-30 07:04:51 +0000

    wip

---
+++ db-test.php
@@ -0,0 +1,8 @@
+<?php
+echo "db test";
+
+echo getenv( 'DB_PORT_3306_TCP_ADDR' );
+
+print_r($_ENV);
+
+print_r($_SERVER);
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "hello. this is PHP";
+echo "hello. this is PHP.";
